5c6082cfd005747fcba06b9980a5f23ab1e16af4
ddprzy37 <122937215+ddprzy37@users.noreply.github.com>
2024-07-16 16:46:46 -0600

    added the registerRoute logic in the src-sw.js file

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -24,7 +24,25 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
+registerRoute(({ request }) => request.mode === 'navigate', ({ event }) =>
+  pageCache.handle({ event })
+);
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50, // Maximum number of entries in cache
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      }),
+    ],
+  })
+);
